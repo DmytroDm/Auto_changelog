ab12a0f9c3dd91959142cab9bd3909c745c6748b
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-01-13 18:05:39 +0200

    fix add commits

--- app/src/main/java/com/verygoodsecurity/auto/auto/changelog/MainActivity.kt
+++ app/src/main/java/com/verygoodsecurity/auto/auto/changelog/MainActivity.kt
@@ -3,10 +3,13 @@ package com.verygoodsecurity.auto.auto.changelog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Test auto changelog
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Test auto changelog
     }
 }
