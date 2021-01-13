cbd37009dcfd25b3b161c0986a217f39317f7e2d
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-01-13 20:01:40 +0200

    fix: Remove Comments

--- app/src/main/java/com/verygoodsecurity/auto/auto/changelog/MainActivity.kt
+++ app/src/main/java/com/verygoodsecurity/auto/auto/changelog/MainActivity.kt
@@ -3,13 +3,10 @@ package com.verygoodsecurity.auto.auto.changelog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-// Comment
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Comment
     }
 }
